4fba3756fb0ca89831d847ebab655722f4490ac4
ysw <1779001867@qq.com>
2020-05-19 15:03:55 +0800

    双指针

--- src/day07/Solution.java
+++ src/day07/Solution.java
@@ -9,37 +9,53 @@ package day07;
 public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int [] nums=new int[]{2,3,1,2,4,3};
-        int i = s.minSubArrayLen(7, nums);
+        int[] nums = new int[]{1, 1};
+        int i = s.minSubArrayLen(3, nums);
         System.out.println(i);
     }
 
     public int minSubArrayLen(int s, int[] nums) {
-        if (nums==null||nums.length==0){
+        //排除null和数组等于0
+        if (nums == null || nums.length == 0) {
             return 0;
         }
-        int length=nums.length-1;
-        int j=0;
-        int num=nums[j];
-        if (num>=s){
-            return 1;
+        //数组只有一个元素，可以直接判断返回
+        if (nums.length == 1) {
+            if (nums[0] >= s) {
+                return 1;
+            } else {
+                return 0;
+            }
         }
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i]>=s){
+        //初始假设length为数组长度
+        int length = nums.length;
+        int index = 0;
+        int sum = 0;
+        boolean flag = false;
+        for (int i = 0; i < nums.length; i++) {
+            //假设取到的元素大于等于s则可以直接返回1
+            if (nums[i] >= s) {
                 return 1;
             }
-            num+=nums[i];
-            if (num>=s){
-                if (i-j<length){
-                    length=i-j;
-                }
-                j++;
-                num=nums[j];
-                i=j;
+            //求取到的元素之和
+            sum += nums[i];
+            //当sum>=s时
+            while (sum >= s) {
+                flag = true;
+                //截取index和i位置的差值，和length进行比较，将较小的复制给length
+                //即更新length
+                length = Math.min((i + 1 - index), length);
+                //减去index位置的元素
+                sum -= nums[index];
+                //index后移
+                index++;
             }
         }
-
-
-        return length;
+        //假设flag为true则说明存在sum>=s的情况，则返回length
+        if (flag) {
+            return length;
+        } else {//flag为假则说明整个数列不存在sum>=s的情况，则返回0
+            return 0;
+        }
     }
 }
