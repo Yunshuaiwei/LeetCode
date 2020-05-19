1ba59ad30dac526bc8cd631d3b3758923bfa277d
ysw <1779001867@qq.com>
2020-05-19 16:01:32 +0800

    链表

---
+++ src/day08/Solution.java
@@ -0,0 +1,48 @@
+package day08;
+
+/**
+ * @Description TODO
+ * @Author YunShuaiWei
+ * @Date 2020/5/19 15:24
+ * @Version
+ **/
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (slow != null) {
+            fast = fast.next;
+            if (fast == null) {
+                return false;
+            } else {
+                fast = fast.next;
+            }
+            if (fast == null) {
+                return false;
+            }
+            if (fast == slow) {
+                return true;
+            }
+            slow = slow.next;
+        }
+        return false;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
