96c883b36c03395c31e6364adf81995ae89deb7d
ysw <1779001867@qq.com>
2020-05-28 17:15:33 +0800

    栈

--- src/day11/Solution.java
+++ src/day11/Solution.java
@@ -5,41 +5,32 @@ import java.util.Stack;
 /**
  * @Description TODO
  * @Author YunShuaiWei
- * @Date 2020/5/26 17:10
+ * @Date 2020/5/28 16:08
  * @Version
  **/
 public class Solution {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int i = s.longestValidParentheses(")(");
+        System.out.println(i);
+    }
 
-    public boolean isValid(String s) {
-        //题目描述，字符串为空返回true
-        if (s == null) {
-            return true;
-        }
-        Stack<Character> stack = new Stack<>();
-        //遍历字符串
+    public int longestValidParentheses(String s) {
+        int length = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            //若c是左括号则入栈
-            if (c == '{' || c == '(' || c == '[') {
-                stack.push(c);
+            if (s.charAt(i) == '(') {
+                stack.push(i);
             } else {
-                //若栈为空则说明匹配到了一个右括号且没有左括号与其匹配，则直接返回false
-                if (stack.isEmpty()) {
-                    return false;
+                stack.pop();
+                if (stack.empty()) {
+                    stack.push(i);
                 } else {
-                    //取栈顶元素
-                    Character p = stack.peek();
-                    //判断是否匹配成功，若成功则pop栈顶元素
-                    if ((p == '{' && c == '}') || (p == '(' && c == ')') || (p == '[' && c == ']')) {
-                        stack.pop();
-                    } else {
-                        //匹配失败则直接返回false
-                        return false;
-                    }
+                    length = Math.max(length, i - stack.peek());
                 }
             }
         }
-        //若栈为空则说明所有括号都匹配成功则返货true，否则返回false，即存在左括号或由括号没有匹配项
-        return stack.isEmpty();
+        return length;
     }
 }
---
+++ src/day11/Solution1.java
@@ -0,0 +1,45 @@
+package day11;
+
+import java.util.Stack;
+
+/**
+ * @Description TODO
+ * @Author YunShuaiWei
+ * @Date 2020/5/26 17:10
+ * @Version
+ **/
+public class Solution1 {
+
+    public boolean isValid(String s) {
+        //题目描述，字符串为空返回true
+        if (s == null) {
+            return true;
+        }
+        Stack<Character> stack = new Stack<>();
+        //遍历字符串
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            //若c是左括号则入栈
+            if (c == '{' || c == '(' || c == '[') {
+                stack.push(c);
+            } else {
+                //若栈为空则说明匹配到了一个右括号且没有左括号与其匹配，则直接返回false
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    //取栈顶元素
+                    Character p = stack.peek();
+                    //判断是否匹配成功，若成功则pop栈顶元素
+                    if ((p == '{' && c == '}') || (p == '(' && c == ')') || (p == '[' && c == ']')) {
+                        stack.pop();
+                    } else {
+                        //匹配失败则直接返回false
+                        return false;
+                    }
+                }
+            }
+        }
+        //若栈为空则说明所有括号都匹配成功则返货true，否则返回false，即存在左括号或由括号没有匹配项
+        return stack.isEmpty();
+    }
+}
