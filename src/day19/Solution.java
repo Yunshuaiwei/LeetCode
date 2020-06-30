2cc88782cbe619e1e9610954bbc7b0f579ab70db
ysw <1779001867@qq.com>
2020-06-30 17:28:42 +0800

    LeetCode

--- src/day19/Solution.java
+++ src/day19/Solution.java
@@ -1,5 +1,6 @@
 package day19;
-import java.util.List;
+
+import java.util.*;
 
 /**
  * @Description TODO
@@ -8,18 +9,40 @@ import java.util.List;
  * @Version
  **/
 public class Solution {
-    public static void main(String[] args) {
-
-    }
 
     public List<List<Integer>> levelOrder(TreeNode root) {
-
-
-        return null;
+        List<List<Integer>> lists = new ArrayList<>();
+        if (root == null) {
+            return lists;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        //先将根节点入队列
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            //保存当前队列的长度，此长度的含义为当前节点所在层的节点数
+            int size = queue.size();
+            //用于保存当前该层的所有节点的值
+            ArrayList<Integer> list = new ArrayList<>();
+            while (size > 0) {
+                //取队首元素
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                //将当前节点的左节点加入队列
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                //将当前节点的右节点加入队列
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                //当前层节点数减一
+                size--;
+            }
+            lists.add(list);
+        }
+        return lists;
     }
-
 }
-
 
 class TreeNode {
     int val;
--- src/day19/Solution1.java
+++ src/day19/Solution1.java
@@ -9,9 +9,27 @@ package day19;
 public class Solution1 {
 
     public boolean isBalanced(TreeNode root) {
-
-
-        return false;
+        if (root == null) {
+            return true;
+        }
+        int hl = height(root.left);
+        int hr = height(root.right);
+        //判断当前节点是否是平衡二叉树，并且需要判断其左右子树是否也时平衡二叉树
+        return Math.abs(hl - hr) <= 1
+                && isBalanced(root.left)
+                && isBalanced(root.right);
     }
 
+    /**
+     * 求树的高度
+     *
+     * @return int
+     * @Param [node]
+     **/
+    private int height(TreeNode node) {
+        if (node == null) {
+            return -1;
+        }
+        return Math.max(height(node.left), height(node.right)) + 1;
+    }
 }
