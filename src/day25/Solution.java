5341e6de0a7623ec01120b5dd53089c0426182f3
ysw <1779001867@qq.com>
2020-07-07 16:39:32 +0800

    LeetCode

---
+++ src/day25/Solution.java
@@ -0,0 +1,27 @@
+package day25;
+
+/**
+ * @Description: TODO
+ * @Author YunShuaiWei
+ * @Date 2020/7/7 15:58
+ * @Version
+ **/
+public class Solution {
+
+    public TreeNode invertTree(TreeNode root) {
+        
+
+        return null;
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
